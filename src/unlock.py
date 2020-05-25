275f5a41b2e43a10aa4e27b4b325ce090c9c149b
Léo <leo951206@gmail.com>
2020-05-25 21:45:53 +0200

    changing README ressource title

--- src/unlock.py
+++ src/unlock.py
@@ -2,7 +2,9 @@ import numpy as np
 import cv2
 
 #cascade "profile"
-FACE_CASCADE = cv2.CascadeClassifier("/home/leo/Documents/Projects/FacialRecognition_Directory/ressources/data/haarcascade_frontalface_alt.xml")
+FACE_CASCADE = cv2.CascadeClassifier("/home/leo/Documents/Projects/FacialRecognition_Directory/ressources/data/haarcascade_frontalface_default.xml")
+R_EYE_CASACADE = cv2.CascadeClassifier("/home/leo/Documents/Projects/FacialRecognition_Directory/ressources/data/haarcascade_righteye_2splits.xml")
+L_EYE_CASACADE = cv2.CascadeClassifier("/home/leo/Documents/Projects/FacialRecognition_Directory/ressources/data/haarcascade_lefteye_2splits.xml")
 
 #defining the VideoCapture object
 cap = cv2.VideoCapture(0)
@@ -21,8 +23,16 @@ while (True):
         #drawing rectangle around the face
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), ) #red rectangle
 
+        Reyes = R_EYE_CASACADE.detectMultiScale(frame_gray, scaleFactor=3, minNeighbors=3)
+        for (rx,ry,rw,rh) in Reyes:
+            cv2.rectangle(frame,(rx+5,ry+5),(rx+rw-5,ry+rh-5),(255,0,0),2)
+        Leyes = L_EYE_CASACADE.detectMultiScale(frame_gray, scaleFactor=3, minNeighbors=3)
+        for (lx,ly,lw,lh) in Leyes:
+            cv2.rectangle(frame,(lx+5,ly+5),(lx+lw-5,ly+lh-5),(255,0,0),2)
+
+
     #Displaying the frame (live video)
-    cv2.imshow('LiveCapture', frame)
+    cv2.imshow('LiveCapture', cv2.flip(frame, 1))
 
     #Checks if the q key is pressed
     if cv2.waitKey(20) & 0xFF == ord('q'):
